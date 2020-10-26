20ad4d5714b68839b4ce995f84ece4f0fc5c5fd0
Stefán Sigurðsson <stefan.sigurdsson@brambolt.com>
2020-10-26 16:21:08 +0100

    Syntactic, ignore.

--- src/main/java/com/brambolt/gradle/api/provider/Properties.java
+++ src/main/java/com/brambolt/gradle/api/provider/Properties.java
@@ -8,17 +8,17 @@ import org.gradle.api.provider.Property;
 
 public class Properties {
 
-    public static <T> Property createProperty(Project project, Class<T> cls) {
+    public static <T> Property<T> createProperty(Project project, Class<T> cls) {
         return project.getObjects().property(cls);
     }
 
-    public static <T> Property createProperty(Project project, Class<T> cls, T value) {
+    public static <T> Property<T> createProperty(Project project, Class<T> cls, T value) {
         Property<T> p = project.getObjects().property(cls);
         p.set(value);
         return p;
     }
 
-    public static <K, V> MapProperty createMapProperty(Project project, Class<K> keyType, Class<V> valueType, Map<K, V> value) {
+    public static <K, V> MapProperty<K, V> createMapProperty(Project project, Class<K> keyType, Class<V> valueType, Map<K, V> value) {
         MapProperty<K, V> p = project.getObjects().mapProperty(keyType, valueType);
         p.set(value);
         return p;
